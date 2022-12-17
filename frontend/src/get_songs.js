1d40db3f8617cfb279b5a60d70eaa86e307c3551
unknown <kevin941308@gmail.com>
2022-12-17 22:12:20 +0800

    fix: getsons func

--- frontend/src/get_songs.js
+++ frontend/src/get_songs.js
@@ -4,22 +4,6 @@ export function GetSongs(url) {
     const [list, setData] = useState([]);
 
     useEffect(() => {
-<<<<<<< HEAD
-        async function getData(url) {
-            const headers = { 
-                'Content-Type': 'application/json',
-            };
-            const response = await fetch(`${url}/`, { 
-                headers: headers,
-                method: 'POST',
-            });
-            // .then(response => response.json())
-            // .catch(error => console.error('Error:', error))
-            // .then(response => console.log('Success:', response))
-
-            const content = await response.json();
-            setData(content);
-=======
         async function getData() {
             const headers = { 
                 'Content-Type': 'application/json',
@@ -32,7 +16,6 @@ export function GetSongs(url) {
 
             const data = await response.json();
             setData(data["message"]);
->>>>>>> a9b7955f819bf3d2933b2bccfd0e94c68fa9046c
         }
         getData();
     }, []);
