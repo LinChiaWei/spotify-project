dafcbd256c61c561d088c3f21a631424152f3334
unknown <kevin941308@gmail.com>
2023-03-08 02:34:41 +0800

    fix db_op

--- backend/models/db_op.py
+++ backend/models/db_op.py
@@ -43,21 +43,21 @@ class DB_operate():
 
         return rows
 
-def update_db(new_data):
-    connection = psycopg2.connect(database="postgres",user="postgres",password="postgres",host="db",port='5432')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM song_list;")
-    print("select Table successfully")
+    def update_db(new_data):
+        connection = psycopg2.connect(database="postgres",user="postgres",password="postgres",host="db",port='5432')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM song_list;")
+        print("select Table successfully")
 
-    db_data = cursor.fetchall()
-    updated_data = update_data(db_data,new_data)
-    print("update data sucessfully")
+        db_data = cursor.fetchall()
+        updated_data = update_data(db_data,new_data)
+        print("update data sucessfully")
 
-    cursor.execute("TRUNCATE TABLE song_list;")
-    print("clear table sucessfully")
+        cursor.execute("TRUNCATE TABLE song_list;")
+        print("clear table sucessfully")
 
-    cursor.executemany("INSERT INTO song_list (song_name, image_url, song_count) VALUES(%s, %s, %s)", updated_data)
-    print("insert table sucessfully")
+        cursor.executemany("INSERT INTO song_list (song_name, image_url, song_count) VALUES(%s, %s, %s)", updated_data)
+        print("insert table sucessfully")
 
-    connection.commit()
-    connection.close()
+        connection.commit()
+        connection.close()
